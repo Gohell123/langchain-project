012e068d2fb3df821b7656b5e24544c9c2934b80
Mandeep Saini <12345678+Gohell123@users.noreply.github.com>
2026-04-23 02:07:03 +0530

    Adding Redis URL from Render

--- api.py
+++ api.py
@@ -7,8 +7,8 @@ import redis
 import json
 from langchain_core.messages import HumanMessage, AIMessage, ToolMessage
 
-redis_client = redis.Redis(host="localhost", port=6379, decode_responses=True)
-#redis_client = redis.from_url(os.getenv("REDIS_URL"))
+#redis_client = redis.Redis(host="localhost", port=6379, decode_responses=True)
+redis_client = redis.from_url(os.getenv("REDIS_URL"))
 
 print("Starting FastAPI app...")
 
